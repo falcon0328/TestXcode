9386c7d7f0d31c3ebdada0aff80b0ef13316348d
FALCON <falcon.x.0328@gmail.com>
2017-10-16 14:55:27 +0900

    Project First Commit!

---
+++ Hoge/Hoge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hoge
+//
+//  Created by falcon@mac on H29/10/16.
+//  Copyright © 平成29年 NagaoLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
